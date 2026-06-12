9947fe4da9e78ef4f1506c827c6e29e37a906cfc
elifaysel <elifayselyildirim03@gmail.com>
2026-06-12 11:12:38 +0300

    Initial Commit

---
+++ MediSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediSync
+//
+//  Created by Elif Aysel YILDIRIM on 12.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediSync/MediSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediSyncApp.swift
+//  MediSync
+//
+//  Created by Elif Aysel YILDIRIM on 12.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MediSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
